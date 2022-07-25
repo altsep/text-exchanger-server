1fc33e81b2d051b3cbb226d0b156446c85687222
l <mx24xnpi4@mozmail.com>
2022-07-25 21:25:57 +0300

    feat: add compression

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
 
+app.use(compression());
 app.use(express.json({ limit: '128kb' }));
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
