7d95f353594d115e74e7d0a84d4f66fe3c7f7dea
l <mx24xnpi4@mozmail.com>
2022-07-29 16:21:13 +0300

    test: add render test for App

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+    const linkElement = screen.getByText(/fragments of text/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
